2833ae1fc25b557995fc87e76d44b93f927495b7
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-01-29 17:36:51 +0000

    ✨ feat: Add options provider

---
+++ src/components/DropdownOption/Provider.jsx
@@ -0,0 +1,61 @@
+import { createContext, useCallback, useEffect, useState } from "react";
+
+const Context = createContext();
+
+export function DropdownProvider({ children }) {
+  const [options, setOptions] = useState([]);
+  const [targetId, setTargetId] = useState(null);
+  const [casheId, setCasheId] = useState(null);
+
+  const registerOption = useCallback(
+    ({
+      id,
+      optionDimensions,
+      optionCenterX,
+      WrappedContent,
+      backgroundHeight,
+    }) => {
+      setOptions((options) => [
+        ...options,
+        {
+          id,
+          optionDimensions,
+          optionCenterX,
+          WrappedContent,
+          backgroundHeight,
+        },
+      ]);
+    },
+    [setOptions]
+  );
+
+  const updateOptionProps = useCallback(
+    (optionId, props) => {
+      setOptions((options) => {
+        options.map((option) => {
+          if (option.id === optionId) {
+          }
+          return option;
+        });
+      });
+    },
+    [setOptions]
+  );
+
+  const getOptionById = useCallback(
+    (id) => options.find((option) => option.id === id),
+    [options]
+  );
+
+  const deleteOptionById = useCallback(
+    (id) =>
+      setOptions((options) => options.filter((option) => option.id !== id)),
+    [setOptions]
+  );
+
+  useEffect(() => {
+    if (targetId) setCasheId(targetId);
+  }, [targetId]);
+
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
+}
